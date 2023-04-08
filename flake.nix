df005f43a5adec609b2a845886e4a3919ed6b6dc
amanibhavam <iam@defn.sh>
2023-04-08 01:31:55 +0000

    use the nixpkgs and flake-utils from pkg.dev

--- flake.nix
+++ flake.nix
@@ -1,36 +1,35 @@
 {
   description = "Convert go.mod/go.sum to Nix packages";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=64c27498901f104a11df646278c4e5c9f4d642db"; # sync with defn/pkg/godev
+  inputs = {
+    pkg.url = github:defn/pkg/0.0.202;
+  };
 
-  inputs.utils.url = "github:numtide/flake-utils?rev=04c1b180862888302ddfb2e3ad9eaa63afc60cf8"; # sync with defn/pkg/dev
+  outputs = inputs: rec {
+    overlays.default = import ./overlay.nix;
 
-  outputs = { self, nixpkgs, utils }:
-    {
-      overlays.default = import ./overlay.nix;
-
-      templates = {
-        app = {
-          path = ./templates/app;
-          description = "Gomod2nix packaged application";
-        };
+    templates = {
+      app = {
+        path = ./templates/app;
+        description = "Gomod2nix packaged application";
       };
-      defaultTemplate = self.templates.app;
+    };
+    defaultTemplate = templates.app;
 
-    } //
-    (utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              self.overlays.default
-            ];
-          };
-        in
-        {
-          packages.default = pkgs.callPackage ./. { };
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        })
-    );
+  } //
+  (inputs.pkg.inputs.dev.inputs.flake-utils.lib.eachDefaultSystem
+    (system:
+      let
+        pkgs = import inputs.pkg.inputs.dev.inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.self.overlays.default
+          ];
+        };
+      in
+      {
+        packages.default = pkgs.callPackage ./. { };
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      })
+  );
 }
